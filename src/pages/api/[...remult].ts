a5040bbfc67ced7011e62597bb09b6b4f6dc55d6
ALLMMIN <chawadon.mont62@sskru.ac.th>
2024-03-04 17:02:36 +0700

    'v1'

--- src/pages/api/[...remult].ts
+++ src/pages/api/[...remult].ts
@@ -1,9 +1,3 @@
-// // src/app/api/[...remult]/route.ts
-
-// import { remultNextApp } from "remult/remult-next";
-// const api = remultNextApp({});
-// export const { POST, PUT, DELETE, GET } = api;
-
 import { remultNext } from "remult/remult-next";
 import { Task } from "../shared/Task";
 import { TasksController } from "../shared/TasksController";
